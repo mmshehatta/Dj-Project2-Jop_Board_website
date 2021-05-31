79d4b20024d9c5a93985c628b15c2bb9b9475013
Mahmoud <mmshehatta@gmail.com>
2021-06-01 00:14:03 +0200

    Implementing Jop App View

--- job/urls.py
+++ job/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path , include
 
-from .views import job_list , job_details
+from .views import(
+    #  job_list , 
+    #  job_details,
+     JobListView,
+     JobDetailsView
+     
+)
 
 urlpatterns = [
-    path('' ,job_list , name='all-jobs'),
-    path('<int:id>' , job_details , name='job-details')
+    path('' ,JobListView.as_view() , name='all-jobs'),
+    path('<int:pk>' , JobDetailsView.as_view() , name='job-details') #note if you will user DetailView generic you must use pk or slug not id...Ok
+    
 ]
--- job/views.py
+++ job/views.py
@@ -1,11 +1,41 @@
 from django.shortcuts import render , HttpResponse
 
+from .models import Jop
+
+from django.views.generic import ListView , DetailView
+
 # Create your views here.
 
+# ****** function view Methodolgy ********
+# def job_list(request):
+    # job_list = Jop.objects.all()
+    # context={
+        # 'jobs':job_list
+    # }
+
+    # return render(request , 'job/job_list.html' ,context)
+# 
+# 
+# def job_details(request,id):
+    # job_details =Jop.objects.get(id=id)
+    # print('='*20)
+    # print(job_details)
+    # context={
+        # 'job':job_details
+    # } 
+    # return render(request , 'job/job_details.html' , context)
+# 
+
+# ****** Class Based view Methodolgy ********
+class JobListView(ListView):
+    model = Jop
+    template_name='job/job_list.html'
+    context_object_name='jobs'
+    # paginate_by=2
+    ordering=['-published_at']
 
 
-def job_list(request):
-    return HttpResponse('<h1> Job List  </h1>')
-
-def job_details(request,id):
-    return HttpResponse(f'<h1> Job num [{id}]</h1>')
+class JobDetailsView(DetailView):
+    model = Jop
+    template_name = 'job/job_details.html'
+    context_object_name = 'job'
